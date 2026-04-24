e3d360514b1a51141594b0f1a538cbe8acb7d7c2
Mickerl_Codes <nanayawwiafe007@gmail.com>
2026-04-24 12:13:57 +0100

    Setup basic Express server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Pulse Check API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
